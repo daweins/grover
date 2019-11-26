c80016e30ab066f2a61c2d7e621198efcc6863cf
David Weinstein (MCS) <daweins@microsoft.com>
2019-11-26 21:52:50 +0000

    NSG Fault Injection

--- AzureFaultInjector/FaultInjectors/FIVM.cs
+++ AzureFaultInjector/FaultInjectors/FIVM.cs
@@ -1,83 +1,55 @@
-using System;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
+﻿using System;
 using System.Collections.Generic;
-using Microsoft.Azure.Management.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
-using Microsoft.Azure.Management.ResourceManager.Fluent;
-using System.Threading;
-using Kusto.Ingest;
-using System.IO;
+using System.Text;
+
 using Microsoft.Azure.Management.Network.Fluent;
+using Microsoft.Extensions.Logging;
+
 
 namespace AzureFaultInjector
 {
-    public static class FIVM
+    class FIVM : FI
     {
-        static Random rnd = new Random();
-        [FunctionName("FIVM")]
-        public static void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, ILogger log)
+        public FIVM (ILogger iLog, Microsoft.Azure.Management.Fluent.IAzure iAzure, string iRGName, string kustoConn, string kustoDBName, string kustoTableName) : base(iLog,iAzure, iRGName, kustoConn, kustoDBName, kustoTableName)
         {
-            log.LogInformation($"Fault Injector - VM function executed at: {DateTime.Now}");
-            string subListStr = Environment.GetEnvironmentVariable("targetSubscriptionIDList");
-            string rgListStr = Environment.GetEnvironmentVariable("targetRGList");
-            string clientId = Environment.GetEnvironmentVariable("clientId");
-            string tenantId = Environment.GetEnvironmentVariable("tenantId");
-            string clientPwd = Environment.GetEnvironmentVariable("clientPassword");
-            string ingestConn = Environment.GetEnvironmentVariable("ingestConn");
-            log.LogInformation($"Params: SubscriptionIDList: {subListStr}; RGList: {rgListStr}");
+            myResourceCollection = iAzure.VirtualMachines.ListByResourceGroup(curRGName);
+            
+            myTargetType = "VM";
+        }
 
-            ServicePrincipalLoginInformation spi = new ServicePrincipalLoginInformation
+        protected override bool turnOn(Microsoft.Azure.Management.ResourceManager.Fluent.Core.IResource curResource)
+        {
+            Microsoft.Azure.Management.Compute.Fluent.IVirtualMachine curVM = (Microsoft.Azure.Management.Compute.Fluent.IVirtualMachine)curResource;
+
+            try
             {
-                ClientId = clientId,
-                ClientSecret = clientPwd
-
-            };
-            AzureCredentials myAzCreds = new AzureCredentials(spi, tenantId, AzureEnvironment.AzureGlobalCloud);
-            var myAz = Azure.Configure().Authenticate(myAzCreds).WithSubscription(subListStr);
-
-            var ingestClient = KustoIngestFactory.CreateQueuedIngestClient(ingestConn);
-            var ingestProps = new KustoIngestionProperties("annandale", "faultInjections");
-            ingestProps.Format = Kusto.Data.Common.DataSourceFormat.csv;
-            using (var memStream = new MemoryStream())
+                curVM.Start();
+                return true;
+            }
+            catch(Exception err)
             {
-                using (var sr = new StreamWriter(memStream))
-                {
-                    bool hasWrites = false;
-                    foreach (string curRGName in rgListStr.Split(","))
-                    {
-                        log.LogInformation($"Iterating over RG {curRGName}");
+                log.LogError($"Error turning on VM {curSubName} -> {curRGName} -> {curVM.Name}: {err}");
+                return false;
+            }
+        }
 
-                        var vmList = myAz.VirtualMachines.ListByResourceGroup(curRGName);
-                        foreach (var curVM in vmList)
-                        {
-                            int nextRnd = rnd.Next(100);
-                            log.LogInformation($"Got VM: {curVM.Name}. Will reboot if {nextRnd} is <= 20");
-                            if (nextRnd <= 20)
-                            {
-                                hasWrites = true;
-                                log.LogInformation($"Turning off VM for 3 minutes: {curVM.Name}");
-                                sr.WriteLine($"{DateTime.UtcNow.ToString()}, {subListStr},{curRGName},{curVM.Name},{"VMFaultInjection"},{1}");
-                                curVM.PowerOff();
-                                Thread.Sleep(180000);
-                                log.LogInformation($"Turning on VM: {curVM.Name}");
-                                sr.WriteLine($"{DateTime.UtcNow.ToString()}, {subListStr},{curRGName},{curVM.Name},{"VMFaultInjection"},{0}");
-                                curVM.StartAsync();
-                                Thread.Sleep(60000);
-                            }
-                        }
-                       
-                    }
-                    if (hasWrites)
-                    {
-                        sr.Flush();
-                        memStream.Seek(0, SeekOrigin.Begin);
-                        ingestClient.IngestFromStream(memStream, ingestProps);
-                    }
-                }
+        protected override bool turnOff(Microsoft.Azure.Management.ResourceManager.Fluent.Core.IResource curResource)
+        {
+            Microsoft.Azure.Management.Compute.Fluent.IVirtualMachine curVM = (Microsoft.Azure.Management.Compute.Fluent.IVirtualMachine)curResource;
+
+            try
+            {
+                curVM.PowerOff();
+                return true;
+            }
+            catch (Exception err)
+            {
+                log.LogError($"Error turning on VM {curSubName} -> {curRGName} -> {curVM.Name}: {err}");
+                return false;
             }
 
         }
+
+
     }
 }
---
+++ AzureFaultInjector/FaultInjectors/FIVNSG.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Azure.Management.Network.Fluent;
+using Microsoft.Extensions.Logging;
+
+
+namespace AzureFaultInjector
+{
+    class FINSG : FI
+    {
+
+        static string myNewRuleName = "FuzzerBlockingRule";
+        static int myNewRulePriority = 100;
+
+
+        public FINSG (ILogger iLog, Microsoft.Azure.Management.Fluent.IAzure iAzure, string iRGName, string kustoConn, string kustoDBName, string kustoTableName) : base(iLog,iAzure, iRGName, kustoConn, kustoDBName, kustoTableName)
+        {
+            myResourceCollection = iAzure.NetworkSecurityGroups.ListByResourceGroup(iRGName);
+            
+            myTargetType = "NSG";
+        }
+
+
+        protected override bool turnOn(Microsoft.Azure.Management.ResourceManager.Fluent.Core.IResource curResource)
+        {
+            Microsoft.Azure.Management.Network.Fluent.INetworkSecurityGroup curNSG = (Microsoft.Azure.Management.Network.Fluent.INetworkSecurityGroup)curResource;
+
+            try
+            {
+                log.LogInformation($"Removing new highest priority blocking rule to NSG {curNSG.Name}");
+
+
+                curNSG.Update().WithoutRule(myNewRuleName).Apply();
+                return true;
+            }
+            catch (Exception err)
+            {
+                log.LogError($"Error unblocking NSG {curSubName} -> {curRGName} -> {curNSG.Name}: {err}");
+                return false;
+            }
+
+        }
+
+        protected override bool turnOff(Microsoft.Azure.Management.ResourceManager.Fluent.Core.IResource curResource)
+        {
+            Microsoft.Azure.Management.Network.Fluent.INetworkSecurityGroup curNSG = (Microsoft.Azure.Management.Network.Fluent.INetworkSecurityGroup)curResource;
+
+            try
+            {
+                // Add a blocking rule
+                log.LogInformation($"Adding new highest priority blocking rule to NSG {curNSG.Name}");
+                curNSG.Update().DefineRule(myNewRuleName)
+                    .DenyInbound()
+                    .FromAnyAddress()
+                    .FromAnyPort()
+                    .ToAnyAddress()
+                    .ToAnyPort()
+                    .WithAnyProtocol()
+                    .WithDescription("Temporary stop everything rule from Azure Fuzzer to simulate a cut network")
+                    .WithPriority(100)
+                    .Attach()
+                    .Apply();
+                return true;
+            }
+            catch (Exception err)
+            {
+                log.LogError($"Error blocking NSG {curSubName} -> {curRGName} -> {curNSG.Name}: {err}");
+                return false;
+            }
+        }
+
+    }
+}
