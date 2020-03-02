bf9698d3cd47b65b0b1ae7673fab51ca9f708259
David Weinstein (MCS) <daweins@microsoft.com>
2020-03-02 16:26:15 +0000

    Moving to cosmosdb to allow for longer transactions

--- AzureFaultInjector.cs
+++ AzureFaultInjector.cs
@@ -7,8 +7,12 @@ using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using System.Threading;
-
-
+using Microsoft.Azure.Management.ResourceGraph;
+using Microsoft.Azure.Management.ResourceGraph.Models;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using Microsoft.Azure.Cosmos;
 
 namespace AzureFaultInjector
 {
@@ -17,7 +21,7 @@ namespace AzureFaultInjector
         static Random rnd = new Random();
 
 
-        [FunctionName("AzureFaultInjector")]
+        [FunctionName("AzureFaultInjector_PopulateErrors")]
         public static void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             try
@@ -31,9 +35,15 @@ namespace AzureFaultInjector
                 string ingestConn   = Environment.GetEnvironmentVariable("ingestConn");
                 int vmFuzzPct       = Convert.ToInt32(Environment.GetEnvironmentVariable("vmFuzzPct"));
                 int nsgFuzzPct      = Convert.ToInt32(Environment.GetEnvironmentVariable("nsgFuzzPct"));
+                string cosmosConn = Environment.GetEnvironmentVariable("cosmosConn");
+                string cosmosDBName = Environment.GetEnvironmentVariable("cosmosDB");
+                string cosmosScheduleContainerName = Environment.GetEnvironmentVariable("cosmosScheduleContainer");
 
                 log.LogInformation($"Params: SubscriptionIDList: {subListStr}; RGList: {rgListStr}");
 
+                // Build the sub list
+                List<string> subList = new List<string>(subListStr.Split(","));
+                log.LogInformation($"Found {subList.Count} subscriptions to target");
                 ServicePrincipalLoginInformation spi = new ServicePrincipalLoginInformation
                 {
                     ClientId = clientId,
@@ -43,6 +53,47 @@ namespace AzureFaultInjector
                 AzureCredentials myAzCreds = new AzureCredentials(spi, tenantId, AzureEnvironment.AzureGlobalCloud);
                 Microsoft.Azure.Management.Fluent.IAzure myAz = Azure.Configure().Authenticate(myAzCreds).WithSubscription(subListStr);
 
+//TODO: test if we need to add max object appSetting to allow > 100 items in the query
+                ResourceGraphClient resourceGraphClient = new ResourceGraphClient(myAzCreds);
+
+                // TODO: Move some of this into the FI objects
+                // Get VMs
+                string vmListQuery = @"Resources 
+                | where tags.allowFaultInjection=~'true'
+                | project id
+                ";
+
+                QueryResponse vmListResponse = resourceGraphClient.Resources(new QueryRequest(subList, vmListQuery));
+                log.LogInformation($"Got VMs: {vmListResponse.Count}");
+                JObject vmJObj = JObject.Parse(vmListResponse.Data.ToString());
+                IList<string> vmIDList = vmJObj.SelectTokens("$.rows[*][0]").Select(s => (string)s).ToList();
+
+                using(CosmosClient cosmosClient = new CosmosClient(cosmosConn))
+                {
+                    Database curDB = cosmosClient.GetDatabase(cosmosDBName);
+                    Container cosmosScheduleContainer = curDB.GetContainer(cosmosScheduleContainerName);
+
+                    // Sample create a VM schedule off/on
+                    ScheduledOperation newOffOp = new ScheduledOperation(DateTime.Now, "Sample VM Off", "off", vmIDList[0]);
+                    ScheduledOperation newOnOp  = new ScheduledOperation(DateTime.Now.AddMinutes(5), "Sample VM On" , "on", vmIDList[0]);
+
+                    // Only create the Off if the On is scheduled successfully
+                    ItemResponse<ScheduledOperation> onCreate = cosmosScheduleContainer.CreateItemAsync(newOnOp, newOnOp.getPartitionKey()).Result;
+                    log.LogInformation($"Created Off schedule: {onCreate.StatusCode} : {newOnOp.ToString()}");
+                    if ((int) onCreate.StatusCode >= 200 && (int)onCreate.StatusCode < 300)
+                    {
+                        ItemResponse<ScheduledOperation> offCreate = cosmosScheduleContainer.CreateItemAsync(newOffOp, newOffOp.getPartitionKey()).Result;
+                        log.LogInformation($"Created Off schedule: {offCreate.StatusCode} : {newOffOp.ToString()}");
+
+                    }
+                    else
+                    {
+                        log.LogError($"Skipping creating the off schedule, as the on schedule failed: {onCreate.StatusCode}");
+                    }
+
+
+                }
+                /*
                 foreach (string curRGName in rgListStr.Split(","))
                 {
                     log.LogInformation($"Iterating over RG {curRGName}");
@@ -52,9 +103,9 @@ namespace AzureFaultInjector
                     FINSG nsgFuzzer = new FINSG(log, myAz, curRGName, ingestConn, "annandale", "faultInjections");
                     nsgFuzzer.Fuzz(25);
                 }
+                */
 
-
-                log.LogInformation($"Fault Injector - VM function finished at: {DateTime.Now}");
+                log.LogInformation($"Fault Injector - Error Populatorfunction finished at: {DateTime.Now}");
 
             }
             catch(Exception err)
---
+++ Structs/ScheduledOperation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+
+namespace AzureFaultInjector
+{
+    public class ScheduledOperation
+    {
+
+        [JsonProperty(PropertyName = "scheduleTime")]
+        public DateTime scheduleTime { get; set; }
+
+
+        [JsonProperty(PropertyName = "opType")]
+        public string opType { get; set; }
+
+
+        [JsonProperty(PropertyName = "target")]
+        public string target { get; set; }
+
+
+        [JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+
+
+        [JsonProperty(PropertyName = "description")]
+        public string description { get; set; }
+
+
+        [JsonProperty(PropertyName = "relatedOps")]
+        public List<string> relatedOps { get; set; }  
+
+
+
+
+    public ScheduledOperation(DateTime iScheduleTime, string iDescription, string iOpType, string iTarget)
+        {
+            scheduleTime = iScheduleTime;
+            opType = iOpType;
+            target = iTarget;
+            description = iDescription;
+            id = Guid.NewGuid().ToString();
+            relatedOps = new List<string>();
+        }
+
+        public PartitionKey getPartitionKey()
+        {
+            // TODO: Think about the partitionkey choice
+            return new PartitionKey(opType);
+        }
+
+        override public string ToString()
+        {
+            return $"ScheduledOperation: id={id}; scheduleTime: {scheduleTime.ToString}; opType: {opType}; target: {target}; description: {description} ";
+
+        }
+        static void createLink(ScheduledOperation a, ScheduledOperation b)
+        {
+            a.relatedOps.Add(b.id);
+            b.relatedOps.Add(a.id);
+
+        }
+    }
+}
